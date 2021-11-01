73d95a9b35fc75c57859fb98f7606d428b48bfa5
mikey999yt <93239843+mikey999yt@users.noreply.github.com>
2021-11-01 21:17:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 userbot = {
- owner: [ "687955238","6281261324817" ],
+ owner: [ "687955238","919556416715" ],
   MONGO_URI: "mongodb+srv://zanDb:veth0581@cluster0.eyx0e.mongodb.net/<mydb>?retryWrites=true&w=majority",
    mess: {
-   wait: "tunggu sedang di proses...",
+   wait: "wait in proses...",
    error: "errorr!!"
    },
    prefix: ["_"],
    gexp: 50,
    limit: 100,
-   packname: 'RixleBot',
-   author: '@arifirazzaq2001',
+   packname: 'TanjiroBot',
+   author: '@tanjiro',
     setting: {
     admin: "only admin",
     group: "only group",
     owner: "owner only pack",
     jadibot: "jadibot only",
-    botadmin: "bot harus menjadi admin"
+    botadmin: "bot must be admin"
     }
 }
 module.exports = userbot
